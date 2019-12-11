4901f0cf6701abd0968af5dcd3219c2746c0ed13
pentamania <tanigami7220@gmail.com>
2019-12-11 16:10:22 +0900

    update: modify log display, fix calculation bug, clean

--- TempoCounter.js
+++ TempoCounter.js
@@ -1,47 +1,86 @@
+var getMedian = (arr)=> {
+  var clone = arr.slice();
+  clone.sort((a, b)=> a - b);
+  const half = clone.length / 2 | 0;
+  if (clone.length%2 === 0) {
+    // even
+    return (clone[half-1] + clone[half]) / 2;
+  } else {
+    // odd
+    return clone[half];
+  }
+}
+var calcAverage = (arr)=> {
+  return arr.reduce((a, b)=> a+b, 0) / arr.length;
+}
+
 /**
  * @class TempoCounter
  */
 export default class TempoCounter {
-  get bpm() {return this._bpm;}
   get deltaLogs() {return this._deltaLogs;}
 
   constructor() {
     this._past = 0;
-    this._bpm = 0;
+    // this._bpm = 0;
     this._deltaLogs = [];
+    this._elapsedTime = 0;
+    this._beatCount = 0;
   }
 
   _logDelta(delta) {
-    // this._deltaLogs.push(delta);
     this._deltaLogs.unshift(delta);
   }
 
   count(cb) {
-    if (!this._past) {
-      this._past = performance.now();
-      this._logDelta(0);
-    } else {
-      let now, delta, sum, avg;
-      now = performance.now();
+    let delta = 0;
 
+    if (!this._past) {
+      // start timer
+      this._past = performance.now();
+    } else {
+      // update timer
+      let now = performance.now();
       delta = now - this._past;
       this._logDelta(delta);
       this._past = now;
-
-      // 差分の平均を得てbpm計算
-      sum = this._deltaLogs.reduce((x,y)=> { return x + y; });
-      avg = sum / this._deltaLogs.length;
-      // this._bpm = (60 / avg*1000);
-      this._bpm = (60000 / avg);
+      this._elapsedTime += delta;
+      this._beatCount++;
 
       // callback
-      if (cb && typeof(cb) === 'function') cb(this._bpm);
+      if (cb && typeof(cb) === 'function')
+        cb(this.bpm);
     }
+
+    return delta;
   }
+
+  // 時間差分全データの平均から
+  getBpmByAverage() {
+    const avg = calcAverage(this._deltaLogs);
+    return 60000 / avg; // 60 / avg*1000
+  }
+
+  // 中央値から計算
+  getBpmByMedian() {
+    return 60000 / getMedian(this._deltaLogs);
+  }
+
+  // 経過時間と拍数から計算
+  getBpmByElapsedTime() {
+    const t = this._elapsedTime / this._beatCount;
+    return 60000 / t;
+  }
+
+  get bpm() { return this.getBpmByElapsedTime(); }
+  // get bpm() { return this.getBpmByMedian(); }
+  // get bpm() { return this.getBpmByAverage(); }
 
   clear() {
     this._deltaLogs = [];
-    this._bpm = 0;
+    // this._bpm = 0;
     this._past = 0;
+    this._elapsedTime = 0;
+    this._beatCount = 0;
   }
 }
--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import TempoCounter from './TempoCounter.js'
-const DELTA_LOG_DISPLAY_NUM = 8;
+var removeChildren = (el)=> {
+  while (el.children.length) {
+    el.removeChild(el.children[0]);
+  }
+}
 
 window.addEventListener('DOMContentLoaded', (e)=> {
 
   const counter = new TempoCounter();
   const bpmOutputEl = document.getElementById('bpm-output');
+  const predictedBpmOutputEl = document.getElementById('bpm-output-predicted');
   const beater = document.getElementById('beater');
   const countLogArea = document.getElementById('count-log-list');
   const resetBtn = document.getElementById('reset-btn');
@@ -12,25 +17,22 @@ window.addEventListener('DOMContentLoaded', (e)=> {
 
   const _tapReaction = ()=> {
     beater.classList.add('scaledown');
-    counter.count((bpm)=> {
+
+    // 計測
+    const deltaTime = counter.count((bpm)=> {
       bpmOutputEl.innerHTML = bpm.toFixed(2);
     });
 
-    // deltaログを表示
-    var li = document.createElement('li');
-    li.innerText = counter.deltaLogs[0].toFixed(2) + " ms";
-    countLogArea.appendChild(li);
-    if (countLogArea.children.length > DELTA_LOG_DISPLAY_NUM) {
-      countLogArea.removeChild(countLogArea.children[0]);
-    };
+    // 時間差分ログを表示
+    const li = document.createElement('li');
+    li.innerText = deltaTime.toFixed(2) + " ms";
+    countLogArea.prepend(li);
   }
 
   const _resetState = ()=> {
     counter.clear();
     bpmOutputEl.innerText = 0;
-    while (countLogArea.children.length) {
-      countLogArea.removeChild(countLogArea.children[0]);
-    }
+    removeChildren(countLogArea);
     resetFlag = false;
     console.log("reset");
   };
@@ -39,11 +41,14 @@ window.addEventListener('DOMContentLoaded', (e)=> {
    * event handlers
    */
   const keyDownHandler = (e)=> {
+    // Shift + Space で計測
     if (e.shiftKey) {
-      // Shift + Space で計測
+      // 必要に応じてリセット
       if (resetFlag) _resetState();
+
       beater.classList.add('shiftPressed');
       // beater.classList.add('heart');
+
       if (e.keyCode === 32) {
       // if (e.code === "Space") {
         e.preventDefault();
@@ -51,6 +56,7 @@ window.addEventListener('DOMContentLoaded', (e)=> {
       }
     }
   }
+
   const keyUpHandler = (e)=>{
     beater.classList.remove('scaledown');
     if (!e.shiftKey) {
@@ -59,6 +65,7 @@ window.addEventListener('DOMContentLoaded', (e)=> {
       // _resetState();
     }
   }
+
   // mouseでカウント
   const mouseDownHandler = (e)=> {
     e.preventDefault();
